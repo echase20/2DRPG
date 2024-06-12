015a04bf071326173c774fe0add66d72c4410d45
echase20 <chase.ev@northeastern.edu>
2024-06-12 16:26:26 -0400

    implemented interactable interface

--- Assets/Scripts/PlayerController2.cs
+++ Assets/Scripts/PlayerController2.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Scripting.APIUpdating;
 using UnityEngine.XR;
 
 public class PlayerController2 : MonoBehaviour
@@ -10,7 +11,11 @@ public class PlayerController2 : MonoBehaviour
     private bool isMoving;
     private Animator animator;
 
+    private Vector2 lastmove;
+
     public LayerMask solidObjects;
+
+    public LayerMask interactableObjects;
 
     private void Awake() {
         animator = GetComponent<Animator>();
@@ -31,6 +36,27 @@ public class PlayerController2 : MonoBehaviour
                 targetPos.x += input.x * speed * Time.deltaTime;
                 targetPos.y += input.y * speed * Time.deltaTime;
                 Vector3 moveDirection = new Vector3(input.x, input.y, 0f);
+                if (input.x > 0)
+                {
+                    lastmove.x = 1;
+                    lastmove.y = 0;
+                }
+                else if (input.x < 0)
+                {
+                    lastmove.x = -1;
+                    lastmove.y = 0;
+                }
+                else if (input.y > 0)
+                {
+                    lastmove.y = 1;
+                    lastmove.x = 0;
+                }
+                else if (input.y < 0)
+                {
+                    lastmove.y = -1;
+                    lastmove.x = 0;
+                }
+
                 
                 if (isWalkable(targetPos))
                     transform.position += moveDirection * speed * Time.deltaTime;
@@ -39,12 +65,27 @@ public class PlayerController2 : MonoBehaviour
         }
         animator.SetBool("isMoving", isMoving);
         isMoving = false;
+
+        if (Input.GetKeyDown(KeyCode.Z))
+            Interact();
     }
-    
+    void Interact() {
+        var lookdirection = new Vector3(lastmove.x, lastmove.y);
+        var whereLooking = transform.position + lookdirection;
+
+        var collider = Physics2D.OverlapCircle(whereLooking, 0.2f, interactableObjects);
+        if (collider != null)
+        {
+            collider.GetComponent<Interactable>()?.Interact();
+        }
+        
+    }
+
+
     private bool isWalkable(Vector3 targetPos)
     {
 
-        if (Physics2D.OverlapCircle(targetPos, 0.2f, solidObjects) != null)
+        if (Physics2D.OverlapCircle(targetPos, 0.2f, solidObjects | interactableObjects) != null)
         {
             return false;
         }
